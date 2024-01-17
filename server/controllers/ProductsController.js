7ff6460e80587625a5d8ab9f9eaf29a15fa3a778
rafiiwicaksono <rafiihsan5@gmail.com>
2024-01-18 00:48:08 +0700

    feat: cloudinary upload image

--- server/controllers/ProductsController.js
+++ server/controllers/ProductsController.js
@@ -1,5 +1,12 @@
 const { ObjectId } = require("mongodb");
 const { db } = require("../configs/mongodb");
+const cloudinary = require('cloudinary').v2;
+const { randomUUID } = require(`crypto`)
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY_CLOUDINARY,
+  api_secret: process.env.API_SECRET_CLOUDINARY
+});
 
 class ProductsController {
   static async getAll(req, res, next) {
@@ -14,7 +21,7 @@ class ProductsController {
 
   static async getDetailProduct(req, res, next) {
     try {
-      const data = await db.collection("products").findOne({_id: new ObjectId(req.params.id)});
+      const data = await db.collection("products").findOne({ _id: new ObjectId(req.params.id) });
       res.status(200).json(data);
     } catch (error) {
       console.log(error);
@@ -24,9 +31,14 @@ class ProductsController {
 
   static async addProduct(req, res, next) {
     try {
-      const {name, image, category, stock, price, discQty, discPercent, isAvailable} = req.body
-      const data = await db.collection("products").insertOne({name, image, category, stock, price, discQty, discPercent, isAvailable, createdAt: new Date(), updatedAt: new Date()});
-      res.status(201).json({message: `Update Product With ID ${data.insertedId} Successfull`});
+      const base64File = Buffer.from(req.file.buffer).toString(`base64`)
+      const dataURI = `data:${req.file.mimetype};base64,${base64File}`
+      const dataImage = await cloudinary.uploader.upload(dataURI,
+        { public_id: `${req.file.originalname}_${randomUUID()}`, folder: `products/image` });
+
+      const { name, category, stock, price, discQty, discPercent, isAvailable } = req.body
+      const data = await db.collection("products").insertOne({ name, image: dataImage.secure_url, category, stock, price, discQty, discPercent, isAvailable, createdAt: new Date(), updatedAt: new Date() });
+      res.status(201).json({ message: `Update Product With ID ${data.insertedId} Successfull` });
     } catch (error) {
       console.log(error);
       next(error);
--- server/routes/productsRouter.js
+++ server/routes/productsRouter.js
@@ -1,9 +1,12 @@
 const ProductsController = require("../controllers/ProductsController");
 
 const router = require(`express`).Router();
+const multer  = require('multer')
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
 
 router.get(`/`, ProductsController.getAll);
 router.get(`/:id`, ProductsController.getDetailProduct);
-router.post(`/`, ProductsController.addProduct);
+router.post(`/`, upload.single('image'), ProductsController.addProduct);
 
 module.exports = router;
